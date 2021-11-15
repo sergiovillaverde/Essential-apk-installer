ce9aaf0d96bf35e63d9199317ee1ff307a7f4a2f
Sergio <sergiovillaverde95@gmail.com>
2021-11-15 18:54:58 +0100

    corrections

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ os.system('cmd /c "adb connect 127.0.0.1:58526"')
 for root, dirs, files in os.walk(adb):
     for app in files:
         if app.endswith('.apk'):
-            print('Installing '+ app.removesuffix('.apk') + ".")
+            print('Installing '+ app.removesuffix('.apk'))
             os.system('cmd /c "adb install '+ app)
-            print(app.removesuffix('.apk') + ' installed.')
+            print(app.removesuffix('.apk') + ' installed')
