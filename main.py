8e135def57ca832c041f0847210e03400612aa62
Sergio <sergiovillaverde95@gmail.com>
2021-11-15 18:51:22 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os, sys
+
+adb = r'C:\Users\sergi\Documents\Android\ADB\platform-tools'
+
+os.chdir(adb)
+os.system('cmd /c "adb connect 127.0.0.1:58526"')
+
+for root, dirs, files in os.walk(adb):
+    for app in files:
+        if app.endswith('.apk'):
+            print('Installing '+ app.removesuffix('.apk') + ".")
+            os.system('cmd /c "adb install '+ app)
+            print(app.removesuffix('.apk') + ' installed.')
