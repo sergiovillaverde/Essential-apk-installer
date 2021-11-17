b3db99de5829d837f69a4bf7b10583eabaaa9627
Sergio <sergiovillaverde95@gmail.com>
2021-11-17 12:01:24 +0100

    apk removal

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ for root, dirs, files in os.walk(adb):
             print('Installing '+ app.removesuffix('.apk'))
             os.system('cmd /c "adb install '+ app)
             print(app.removesuffix('.apk') + ' installed')
+            print('Do you want to remove ' + app + '?')
+            respuesta = input()
+            if respuesta == 'yes' or respuesta == 'y' or respuesta == 'Yes' or respuesta == 'Y':
+                os.remove(app)
+                print(app + ' was removed.')
+                print('############')
+            else:
+                print('############')
